15a06b335f27dd87e410446e754fe9ad95144c07
Cytro54 <filoue@s212-07.eaux-claires.net>
2022-03-28 10:59:22 +0200

    Modification du module IA

--- modules/p4_ia.py
+++ modules/p4_ia.py
@@ -5,12 +5,25 @@ import random
 COLONNES = 7
 LIGNES = 6
 
-def calculer_meilleur_move(plateau):
+DIFFICULTE_ALEATOIRE = 0
+DIFFICULTE_FACILE = 1
+
+def calculer_meilleur_move(plateau, difficulte):
     """
     Calcule le meilleur mouvement a jouer pour un plateau donné
 
-    Entrée : Un plateau implémente par un tableau de tableau. 
+    Entree :
+     - `plateau`: Object 'P4_game'
+     - `difficulté`: Représente le niveau de difficulté; peut etre;
+        - 0: Aléatoire: Choisit une case au hasard
+        - 1: Facile: Un algorithme de MinMax facile
+
     Sortie : Le numero de colonne sur lequel il faut placer le pion controllé par l'IA
     """
     # HAHA COMMENT C NUL
-    return random.randint(0, COLONNES)
+    if difficulte == DIFFICULTE_ALEATOIRE:
+        return random.randint(0, COLONNES)
+    
+    if difficulte == DIFFICULTE_FACILE:
+        raise "Pas encore implémenté"
+
