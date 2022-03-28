2ad89773deefecbeccab28c7ba15b392b09f6dfe
Osama LABABIDI <lababido@s212-02.eaux-claires.net>
2022-03-28 11:40:35 +0200

    s

--- modules/p4_tkinter.py
+++ modules/p4_tkinter.py
@@ -7,17 +7,8 @@ J2 = 2  # case sélectionnée par le joueur 2
 INACTIF = 0
 EN_COURS = 1
 FINI = 2
- 
-# texte à afficher - couleur - couleur au survol
-APPARENCE = {
-    J1: ('1', '#EE5555', '#FF6666'),
-    J2: ('2', '#5555EE', '#6666FF'),
-    VD: ('x', '#222222', '#444444')
-}
- 
-def initialiser_les_donnees(d, c):
-    pass
- 
-def coup_valide(d, c, x):
-    return True
- 
+
+
+for ligne in range(5):
+    for colonne in range(5):
+        Button(fenetre, text='Ls-Cs' (ligne, colonne), borderwidth=1).grid(row=ligne, column=colonne)
