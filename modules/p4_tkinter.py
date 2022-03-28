c465e24a3cb4815cb7aeb17e122d70fa0bee2f88
Osama LABABIDI <lababido@s212-02.eaux-claires.net>
2022-03-28 11:50:40 +0200

    s

--- modules/p4_tkinter.py
+++ modules/p4_tkinter.py
@@ -1,14 +1,38 @@
-import tkinter as tk
- 
+from tkinter import * 
+  
+class IHM(Frame):
+    def __init__(self, fenetre, height, width): 
+        Frame.__init__(self, fenetre) 
+        self.numberLines = height 
+        self.numberColumns = width 
+        self.pack(fill=BOTH) 
+        self.data = list() 
+        for i in range(self.numberLines): 
+            line = list() 
+            for j in range(self.numberColumns): 
+                cell = Entry(self) 
+                cell.insert(0, 0) 
+                line.append(cell) 
+                cell.grid(row = i, column = j) 
+            self.data.append(line) 
+  
+        self.results = list() 
+        for i in range(self.numberColumns): 
+            cell = Entry(self) 
+            cell.insert(0, 0) 
+            cell.grid(row = self.numberLines, column = i) 
+            self.results.append(cell) 
+        self.buttonSum =  Button(self, text="somme des colonnes", fg="red", command=self.sumCol) 
+        self.buttonSum.grid(row = self.numberLines, column = self.numberColumns) 
+  
+    def sumCol(self): 
+        for j in range(self.numberColumns): 
+            result = int(0) 
+            for i in range(self.numberLines): 
+                result += int(self.data[i][j].get()) 
+            self.results[j].delete(0, END) 
+            self.results[j].insert(0, result)
 
-VD = 0  # case vide
-J1 = 1  # case sélectionnée par le joueur 1
-J2 = 2  # case sélectionnée par le joueur 2
-INACTIF = 0
-EN_COURS = 1
-FINI = 2
-
-
-for ligne in range(5):
-    for colonne in range(5):
-        Button(fenetre, text='Ls-Cs' (ligne, colonne), borderwidth=1).grid(row=ligne, column=colonne)
+fenetre = Tk() 
+interface = IHM(fenetre, 6, 7) 
+interface.mainloop()
