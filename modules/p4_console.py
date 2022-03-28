3e83e0889e24153a8f728c05957f2b5053383c06
TheGreatestRock <vieux-mv@s212-01.eaux-claires.net>
2022-03-28 11:56:50 +0200

    ajout de joueur1 et de joueur2, mise a jour de affiche

--- modules/p4_console.py
+++ modules/p4_console.py
@@ -2,22 +2,30 @@ class p4console:
     def __init__(self):
 
     def affiche(self,plateau):
+        print("╭---------------------╮")
+        print("|     Puissance 4     |")
+        print("|---------------------|")
         for i in plateau:
+            print("| ", end="")
             for y in range(len(i)):
                 if i[y] == 0
                     print('__ ', end="")
                 if i[y] == 1
-                    print(f'{self.joueur1.motif} ', end="")
+                    print(f'{joueur.j1.motif} ', end="")
                 if i[y] == 2
-                    print(f'{self.joueur2.motif} ', end="")
-
+                    print(f'{joueur.j2.motif} ', end="")
+                print(" |", end="")
+        print("╰---------------------╯")
 
 #classe joueur
 class joueur:
-    def __init__(self,couleur,motif,nom_joueur):
+    def __init__(self):
+        pass
+    def j1(self,couleur,motif,nom_joueur)
         self.couleur = couleur
-        self.nom_joueur = nom_joueur
-        self.score = 0
+        self.motif = motif
+    def j2(self,couleur,motif,nom_joueur)
+
     def gagne(self,nom_joueur):
         self.score +=1
 
