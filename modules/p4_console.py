4e82be884ef8ab387b85bd52c3d426f76efcebce
TheGreatestRock <vieux-mv@s212-01.eaux-claires.net>
2022-03-28 11:34:19 +0200

    debut de la fonction affiche

--- modules/p4_console.py
+++ modules/p4_console.py
@@ -1,10 +1,20 @@
 class p4console:
     def __init__(self):
-        pass
+
+    def affiche(self,plateau):
+        for i in plateau:
+            for y in range(len(i)):
+                if i[y] == 0
+                    print('__ ', end="")
+                if i[y] == 1
+                    print(f'{self.joueur1.motif} ', end="")
+                if i[y] == 2
+                    print(f'{self.joueur2.motif} ', end="")
+
 
 #classe joueur
 class joueur:
-    def __init__(self,couleur,nom_joueur):
+    def __init__(self,couleur,motif,nom_joueur):
         self.couleur = couleur
         self.nom_joueur = nom_joueur
         self.score = 0
@@ -13,11 +23,4 @@ class joueur:
 
 
 
-
-__ __ __ __ __ __ __
-__ __ __ __ __ __ __
-__ __ __ __ __ __ __
-__ __ __ __ __ __ __
-__ __ __ __ __ __ __
-__ __ __ __ __ __ __
 
