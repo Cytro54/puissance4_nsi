e3efa6b2dced56effed1c5ea2c2a354ba43bf056
TheGreatestRock <vieux-mv@s212-01.eaux-claires.net>
2022-03-28 10:54:37 +0200

    test

--- modules/p4_console.py
+++ modules/p4_console.py
@@ -1,6 +1,8 @@
 class p4console():
     def __init__(self):
-
+        pass
 class joueur(couleur,nom_joueur):
     def __init__(self):
-        
+        self.couleur = couleur
+        self.nom_joueur = nom_joueur
+    
