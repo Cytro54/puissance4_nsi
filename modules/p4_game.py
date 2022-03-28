76b38e2e3de13bc66f450aa28e9082766238e969
Yanis CHERIBET <cheribey@s212-08.eaux-claires.net>
2022-03-28 11:52:53 +0200

    methode jeu, creation du tableau

--- modules/p4_game.py
+++ modules/p4_game.py
@@ -1,21 +1,23 @@
 #variable principale
-nb_lignes = 6
-nb_colonnes = 7
-nb_lignes_gagnant = 4
+nb_line = 6
+nb_col = 7
+nb_winer_line = 4
 nb_joueurs = 2
 vide, joueur1, joueur2 = 0, 1, 2
 
 class P4_game():
     def __init__(self):
         self.plateau = []
-        for ligne in range(nb_lignes+2): # +2 cases pour les bords
-            self.plateau.append(nb_colonnes+2) # +2 cases pour les bords
         self.tour = 0
 
     def jeu(self):
-        pass
+        for line in range(nb_line):
+            nvline = []
+            for col in range(nb_col):
+                nvline.append(col * 0)
+            self.plateau.append(nvline)
     
-    def jeu_possible(self):
+    def jeu_possible(self,incolonne):
         pass
 
     def get_case(self,ligne,colonnes):
