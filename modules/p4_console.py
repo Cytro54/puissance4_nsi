a27af8517871d1bb69206f8aa29b99ec51f395a6
TheGreatestRock <vieux-mv@s212-01.eaux-claires.net>
2022-03-30 11:27:15 +0200

    j1 et j2 son maintenant des dictionnaires, on peut maintenant afficher le score et quelques corrections du code ont été faites

--- modules/p4_console.py
+++ modules/p4_console.py
@@ -4,35 +4,50 @@ class p4_console:
     def __init__(self):
         pass
     def affiche(self,plateau):
-        print(f"╭--------------------╮     Score {joueur.j1.nom} : {joueur.j1.score}           ")
-        print(f"|     Puissance 4    |     Score {joueur.j2.nom} : {joueur.j2.score}           ")
-        print(f"|--------------------|                    ")
+        print(f'╭--------------------╮     Score {j1['nom']} : {j1['score']}')
+        print(f'|     Puissance 4    |     Score {j2['nom']} : {j2['score']}')
+        print('|--------------------|')
         for i in plateau:
-            print("| ", end="")
+            print('| ', end=')
             for y in range(len(i)):
                 if i[y] == 0 :
-                    print('__|', end="")
+                    print('__|', end=')
                 if i[y] == 1 :
-                    print(f'{joueur.j1.motif}|', end="")
+                    print(f'{j1['motif']}|', end=')
                 if i[y] == 2 :
-                    print(f'{joueur.j2.motif}|', end="")
-                print(" |", end="")
-        print("╰--------------------╯")
+                    print(f'{j2['motif']}|', end=')
+                print(' |', end=')
+        print('╰--------------------╯')
 
 #classe joueur
 class joueur:
     def __init__(self):
-        self.motif = ""
+        self.motif = '
         self.score = 0
-        self.nom = ""
-    
-    def j1(self,motif,nom_joueur):
-        self.motif = motif
-        self.nom = nom_joueur
+        self.nom = '
+        self.j1 = {}
+        self.j2 = {}
 
-    def j2(self,motif,nom_joueur):
-        self.motif = motif
-        self.nom = nom_joueur
+    def debut_jeu(self):
+        nb_joueurs = 0
+        while nb_joueurs != 1 or nb_joueurs != !2 :
+            print('Mode de jeux :')
+            print('1: joueur contre IA')
+            print('2: joueur contre joueur')
+            nb_joueurs = int(input())
+            if nb_joueurs != 1 or nb_joueurs != 2 :
+                print('erreur')
+        if nb_joueurs == 1 :
+            j1{'nom'} = input('nom du joueur :')
+            j2{'nom'} = 'Viktor Nikiforov'    
+            j1{'score'} = 0
+            j2{'score'} = 0
+
+        if nb_joueurs == 2 :
+            j1{'nom'} = input('nom du joueur 1 :')
+            j2{'nom'} = input('nom du joueur 2 :')
+            j1{'score'} = 0
+            j2{'score'} = 0
 
     def gagne(self,nom_joueur):
         self.score +=1
