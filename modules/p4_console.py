4154b479be7ddc36abbd51a4387136389ba1719f
TheGreatestRock <vieux-mv@s212-01.eaux-claires.net>
2022-03-30 10:26:34 +0200

    mise a jour de la classe joueur et correction de quelques erreur

--- modules/p4_console.py
+++ modules/p4_console.py
@@ -2,7 +2,7 @@ import P4_game
 
 class p4_console:
     def __init__(self):
-
+        pass
     def affiche(self,plateau):
         print("╭---------------------╮")
         print("|     Puissance 4     |")
@@ -10,11 +10,11 @@ class p4_console:
         for i in plateau:
             print("| ", end="")
             for y in range(len(i)):
-                if i[y] == 0
+                if i[y] == 0 :
                     print('__ ', end="")
-                if i[y] == 1
+                if i[y] == 1 :
                     print(f'{joueur.j1.motif} ', end="")
-                if i[y] == 2
+                if i[y] == 2 :
                     print(f'{joueur.j2.motif} ', end="")
                 print(" |", end="")
         print("╰---------------------╯")
@@ -22,11 +22,15 @@ class p4_console:
 #classe joueur
 class joueur:
     def __init__(self):
-        pass
-    def j1(self,motif,nom_joueur)
+        self.motif = ""
+        self.score = 0
+    
+    def j1(self,motif,nom_joueur):
         self.motif = motif
-    def j2(self,motif,nom_joueur)
+    
+    def j2(self,motif,nom_joueur):
         self.motif = motif
+    
     def gagne(self,nom_joueur):
         self.score +=1
 
