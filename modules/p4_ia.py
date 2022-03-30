734890304d4ce62de4833a3e540d84637fc78a9c
Cytro54 <filoue@s212-07.eaux-claires.net>
2022-03-30 11:51:22 +0200

    L'IA Marche :)))) python3 modules/p4_ia.pypython3 modules/p4_ia.pypython3 modules/p4_ia.py

--- modules/p4_ia.py
+++ modules/p4_ia.py
@@ -10,43 +10,95 @@ CASE_NEUTRE = 0
 DIFFICULTE_ALEATOIRE = 0
 DIFFICULTE_FACILE = 1
 
-def __estimer_avantage(plateau, joueur, ligne, colonne, longueur):
+def __estimer_avantage_vectoriel(
+    plateau, 
+    joueur, 
+    origine_ligne, 
+    origine_colonne,
+    vecteur_ligne,
+    vecteur_colonne, 
+    norme_vecteur):
+
+    ligne = origine_ligne + norme_vecteur * vecteur_ligne     
+    colonne = origine_colonne + norme_vecteur * vecteur_colonne
+    norme_vecteur += 1
+    somme = 0
+    
+    if ligne < 0 or colonne < 0:
+        # En dehors du plateau; 
+        return 0
+
+    try:
+        case = plateau[ligne][colonne]
+        print(f"({ligne}, {colonne}): {case}")
+    except IndexError:
+        # En dehors du plateau ? ; la ligne s'arette ici.
+        return 0
+
+    if case != CASE_NEUTRE and case != joueur:
+        # Si la case est pas neutre ou de nous, alors la ligne s'arette ici. 
+        return 0
+
+    if case == joueur:
+        # La case est a nous :), ajoutons 1 au compteur
+        somme += 1
+
+    # Maintenant, calculons cette somme pour la case adjacente suivant le vecteur.
+    #print(f"{norme_vecteur} > ({ligne},{colonne})")
+    somme += __estimer_avantage_vectoriel(
+        plateau, 
+        joueur, 
+        origine_ligne, 
+        origine_colonne, 
+        vecteur_ligne, 
+        vecteur_colonne, 
+        norme_vecteur)
+
+    return somme
+
+def __estimer_avantage(
+    plateau, 
+    joueur, 
+    ligne, 
+    colonne):
 
     #    . . . . . 
     #    . 0 X X X
     #    0 X 0-0-0
     somme = 0
-    if longueur == 0:
-        return 0
-    try:
-        plateau[ligne][colonne]
-    except IndexError:
-        return 0
-        
-    if plateau[ligne][colonne] == joueur:
-        somme += 1
-    if plateau[ligne][colonne] == joueur or plateau[ligne][colonne] == CASE_NEUTRE:
-        
-        somme += __estimer_avantage(plateau, joueur, ligne - 1, colonne - 1, longueur - 1)
-        somme += __estimer_avantage(plateau, joueur, ligne - 1, colonne, longueur - 1)
-        somme += __estimer_avantage(plateau, joueur, ligne - 1, colonne + 1, longueur - 1)
 
-        somme += __estimer_avantage(plateau, joueur, ligne + 1, colonne - 1, longueur - 1)
-        somme += __estimer_avantage(plateau, joueur, ligne + 1, colonne, longueur - 1)
-        somme += __estimer_avantage(plateau, joueur, ligne + 1, colonne + 1, longueur - 1)     
-        
-        somme += __estimer_avantage(plateau, joueur, ligne, colonne - 1, longueur - 1)
-        somme += __estimer_avantage(plateau, joueur, ligne, colonne + 1, longueur - 1)
+    def estim_rapide(vecteur_ligne, vecteur_colonne):
+        print(f" === Trajet === ")
+        ajout = __estimer_avantage_vectoriel(plateau, joueur, ligne, colonne, vecteur_ligne, vecteur_colonne, 0)
+        if ajout >= LONGEUR_LIGNE_VICTORIEUSE:
+            raise RuntimeWarning(f"Gagné : Depart de x({ligne}, {colonne}) Vecteur v({vecteur_ligne}, {vecteur_colonne})")
+        else:
+            print(" ============== ")
+            return ajout
 
-        return somme
-    else:
-        return 0
+    # Calculer pour toutes les lignes possibles
+
+    somme += estim_rapide(0, 1)
+    somme += estim_rapide(0, -1)
+
+    somme += estim_rapide(1, 1)
+    somme += estim_rapide(1, -1)
+    somme += estim_rapide(1, 0)
+
+    somme += estim_rapide(-1, 1)
+    somme += estim_rapide(-1, -1)
+    somme += estim_rapide(-1, 0)
+
+    return somme
 
 def __estimer_avantages(plateau, joueur):
     somme = 0
-    for ligne in range(0, LIGNES):
-        for colone in range(0, COLONNES):
-            somme += __estimer_avantage(plateau, joueur, ligne, colone, LONGEUR_LIGNE_VICTORIEUSE)
+    for ligne in range(0, COLONNES - 1):
+        for colonne in range(0, LIGNES - 1):
+            if plateau[ligne][colonne] == joueur:
+                somme += __estimer_avantage(plateau, joueur, ligne, colonne)
+
+    return somme
 
 def __dump_plateau(game):
     tableau = [[0 for col in range(COLONNES)] for ligne in range(LIGNES)]
@@ -77,37 +129,63 @@ def calculer_meilleur_move(game, difficulte):
 # Asserts, Ou comment ne pas tester une IA
 if __name__ == "__main__":
     # Trucs utiles
-    def assert_avantage(plateau, ligne, colonne, expected1, expected2):
-        avantage1 = __estimer_avantage(plateau, 1, ligne, colonne, 4)
-        avantage2 = __estimer_avantage(plateau, 2, ligne, colonne, 4)
+    def assert_avantage(plateau, joueur_gagnant):
+        avantage1 = __estimer_avantages(plateau, 1)
+        avantage2 = __estimer_avantages(plateau, 2)
         print(f"Avantage 1: {avantage1}")
         print(f"Avantage 2: {avantage2}")
-        assert avantage1 == expected1
-        assert avantage2 == expected2
+        if joueur_gagnant == 1:
+            assert avantage1 >= avantage2
+        if joueur_gagnant == 2:
+            assert avantage2 >= avantage1
 
     
     # Jeu Nul
     PLATEAU_1 = [
-        [0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],     # |
+        [0, 0, 0, 0, 0, 0],     # | "Ligne"
+        [0, 0, 0, 0, 0, 0],     # V
+        [0, 0, 0, 0, 0, 0],     # + ----->
+        [0, 0, 0, 0, 0, 0],     #    "Colonne"
+        [0, 0, 0, 0, 0, 0],     #
     ]
 
     print(" == Plateau Nul == ")
-    assert_avantage(PLATEAU_1, 0, 0, 0, 0) # Personne ne gagne ici
+    assert_avantage(PLATEAU_1, 1)
 
     # Jeu avec avantage pour 1
-    PLATEAU_1 = [
+    PLATEAU_2 = [
         [0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0],
-        [0, 0, 1, 2, 0, 0],
-        [0, 1, 1, 1, 2, 0],
+        [0, 0, 0, 0, 1, 0],
+        [0, 0, 0, 0, 1, 0],
+        [0, 1, 0, 1, 1, 0],
     ]
 
     print(" == Avantage de 1 == ")
-    assert_avantage(PLATEAU_1, LIGNES, 3, 0, 0) # Personne ne gagne ici
+    try:
+        assert_avantage(PLATEAU_2, 1)
+    except RuntimeWarning:
+        # Normalement; l'estimation deverait dire que il a gagné
+        # Donc on s'attend a une erreur
+        pass
+
+    # Jeu avec victoire pour 1
+    PLATEAU_3 = [
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 0],
+        [0, 0, 0, 0, 1, 0],
+        [0, 0, 0, 0, 1, 0],
+        [0, 1, 0, 1, 1, 0],
+    ]
+
+    print(" == Victoire de 1 == ")
+    try:
+        assert_avantage(PLATEAU_3, 1)
+        raise "C'est censé crasher xd"
+    except RuntimeWarning:
+        # Normalement; l'estimation deverait dire que il a gagné
+        # Donc on s'attend a une erreur
+        pass
