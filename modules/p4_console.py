cc4333050df7a66b55d33458130db3109d693f44
TheGreatestRock <vieux-mv@s212-01.eaux-claires.net>
2022-03-30 12:04:59 +0200

    correction de beaucoup d'erreurs ajout de chose dans debut_jeu

--- modules/p4_console.py
+++ modules/p4_console.py
@@ -4,32 +4,33 @@ class p4_console:
     def __init__(self):
         pass
     def affiche(self,plateau):
-        print(f"╭--------------------╮     Score {j1['nom']} : {j1['score']}")
-        print(f"|     Puissance 4    |     Score {j2['nom']} : {j2['score']}")
-        print("|--------------------|")
+        print(f"┏━━━━━━━━━━━━━━━━━━━━━━━━━━━┓     Score {j1['nom']} : {j1['score']}")
+        print(f"┃         Puissance 4       ┃     Score {j2['nom']} : {j2['score']}")
+        print("┃━━━━━━━━━━━━━━━━━━━━━━━━━━━┃")
         for i in plateau:
-            print("| ", end=")
+            print("┃ ", end="")
             for y in range(len(i)):
                 if i[y] == 0 :
-                    print("__|", end=")
+                    print("___┃", end="")
                 if i[y] == 1 :
-                    print(f"{j1['motif']}|", end=")
+                    print(f"{j1['motif']}┃", end="")
                 if i[y] == 2 :
-                    print(f"{j2['motif']}|", end=")
-                print(" |", end=")
-        print("╰--------------------╯")
+                    print(f"{j2['motif']}┃", end="")
+                print(" ┃")
+                print("┃━━━╋━━━╋━━━╋━━━╋━━━╋━━━╋━━━┃")
+        print("┃━━━━━━━━━━━━━━━━━━━━━━━━━━━┃")
+        print("┃ 1 ┃ 2 ┃ 3 ┃ 4 ┃ 5 ┃ 6 ┃ 7 ┃")
+        print("┗━━━━━━━━━━━━━━━━━━━━━━━━━━━┛")
 
 #classe joueur
 class joueur:
     def __init__(self):
         self.motif = ""
         self.nom = ""
-        self.j1 = {}
-        self.j2 = {}
 
     def debut_jeu(self):
         nb_joueurs = 0
-        while nb_joueurs != 1 or nb_joueurs != !2 :
+        while nb_joueurs != 1 or nb_joueurs != 2 :
             print("Mode de jeux :")
             print("1: joueur contre IA")
             print("2: joueur contre joueur")
@@ -37,16 +38,30 @@ class joueur:
             if nb_joueurs != 1 or nb_joueurs != 2 :
                 print("erreur")
         if nb_joueurs == 1 :
-            j1{"nom"} = input("nom du joueur :")
-            j2{"nom"} = "Viktor Nikiforov"    
-            j1{"score"} = 0
-            j2{"score"} = 0
+            j1 = {}
+            j2 = {}
+            j1["nom"] = input("nom du joueur :")
+            j2["nom"] = "Viktor Nikiforov"    
+            j1["score"] = 0
+            j2["score"] = 0
+            lmotif = ["⯀","⯁","⯂","⯄","⯅"]
+            print("motif du joueur 1 :")
+            for i in range(len(lmotif)):
+                print(f"{i} : {lmotif[i]}")
+            motif1 = lmotif[int(input())]
+            lmotif.remove(motif1)
+            print("motif du joueur 1 :")
+            for i in range(len(lmotif)):
+                print(f"{i} : {lmotif[i]}")
+            motif2 = lmotif[int(input())]
+            j1["motif"] = motif1
+            j2["motif"] = motif2
 
         if nb_joueurs == 2 :
-            j1{"nom"} = input("nom du joueur 1 :")
-            j2{"nom"} = input("nom du joueur 2 :")
-            j1{"score"} = 0
-            j2{"score"} = 0
+            j1["nom"] = input("nom du joueur 1 :")
+            j2["nom"] = input("nom du joueur 2 :")
+            j1["score"] = 0
+            j2["score"] = 0
 
 
 
