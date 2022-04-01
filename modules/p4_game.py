eeb53886f35dd59c476a8e6e2d03d9a32adb5890
Yanis CHERIBET <cheribey@s212-08.eaux-claires.net>
2022-04-01 11:51:52 +0200

    oui

--- modules/p4_game.py
+++ modules/p4_game.py
@@ -1,20 +1,20 @@
 #variables principales
-nb_line = 6
-nb_col = 7
-nb_winer_line = 4
+nb_line = 6 # hauteur du plateau
+nb_col = 7 # largeur du plateau
+nb_winer_line = 4 
 nb_joueurs = 2
-vide, joueur1, joueur2 = 0, 1, 2
-j1_score = 0
-j2_score =0
+vide, j1, j2 = 0, 1, 2 # diférenciation des cases du joueur1, du joueur 2 et des cases vides
+j1_score, j2_score = 0, 0
 
 class P4_game():
    """
-   classe qui va s'occuper de la creation du jeu
+   classe qui va s'occuper de la création du jeu
    """ 
     def __init__(self):
         self.plateau = []
-        #si le tour est un nombre paire le joueur 1 joue, sinon c'est le joueur 2 qui joue  
-        self.tour = 0
+        self.tour = 0 #si le tour est un nombre paire le joueur 1 joue, sinon c'est le joueur 2 qui joue  
+
+
 
     def jeu(self):
         """
@@ -27,7 +27,11 @@ class P4_game():
             self.plateau.append(nvline)
 
         return self.plateau
-    
+
+    def get_case(line,col):
+        return self.plateau[col][line]
+
+
     def jeu_possible(self,col):
         """
         entré: numero de la colonne (1 à 7)
@@ -45,32 +49,49 @@ class P4_game():
             return True
              
 
-    def placer(self,col):
+    def placer(self,col):if self.tour % 2 == 0:
         """
         entrée: le numéro de la colonne
         sortie: le jeton correspondant au joueurs 1 ou 2 serra ajouté au dessus des autres jetons de la colonne
         """
-        if self.tour % 2 == 0:
+        if self.jeu_possible() = False:
+            print("la colonne est pleine")
+
+        elif self.tour % 2 == 0:
             for i in self.plateau[col]:
-                while self.plateau[col][i] >= nb_line:
-                    if i != 0:
-                        self.plateau[col][i-1] = 1
-                    else:
-                        pass
+                while i >= nb_line:
+                    if self.plateau[col][i] = 0:
+                        self.plateau[col][i] = 1
+
             self.tour +=1
 
         else:
             for i in self.plateau[col]:
-                while self.plateau[col][i] >= nb_line:
-                    if i != 0:
-                        self.plateau[col][i-1] = 2
-                    else:
-                        pass
+                while i >= nb_line:
+                    if self.plateau[col][i] = 0:
+                        self.plateau[col][i] = 2
+
             self.tour +=1
         return self.tour, self.plateau
 
     def victoire(self):
-        pass
+        """
+        fonction qui va déterminer qui à gagner la partie et qui va ajouter 1 au score du gagnant
+        """
+        while:
+
+            if tour %2 = 0:
+                if somme = nb_winer_line:
+                    return j1_score +1
+            else:
+                if somme = nb_winer_line:
+                    return j1_score +1
+        
+
+
     
     def get_score(self):
-        return j1_score, j2_score
+        """
+        renvoie le score des deux joueurs
+        """
+        return self.j1_score, self.j2_score
