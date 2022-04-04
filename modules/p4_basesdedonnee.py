85f5a8b1a37c38ef9bc8eb71190ded0083bef251
TheGreatestRock <vieux-mv@s212-01.eaux-claires.net>
2022-04-04 12:04:09 +0200

    quelque modif

--- modules/p4_basesdedonnee.py
+++ modules/p4_basesdedonnee.py
@@ -1,25 +1,36 @@
 import sqlite3
 			
 class p4_basesdedonnee():
-    def __init__():
-        conn = sqlite3.connect('base_de_donnees.db')
-        cur = conn.cursor()
+    def __init__(self):
+        self.conn = sqlite3.connect('base_de_donnees.db')
+        self.cur = conn.cursor()
         
 
     def ajoutdejoueuroumodificationdelabasededonee(self, nom_joueur, score):
-        nvx_data = (nom_joueur, score))
-        cur.execute("INSERT INTO TOP_SCORE(nom,score) VALUES(?, ?)", nvx_data)
-        conn.commit()
+        nvx_data = (nom_joueur, score)
+        self.cur.execute("INSERT INTO TOP_SCORE(nom,score) VALUES(?, ?)", nvx_data)
+        self.conn.commit()
 
 
     def recupererlesmeilleursscores(self):
 
-        conn = sqlite3.connect('base_de_donnees.db')
-        cur = conn.cursor()
-        cur.execute('SELECT * FROM TOP_SCORE')
-        conn.commit()
-        listescore = cur.fetchall()
+        self.cur.execute('SELECT * FROM TOP_SCORE')
+        self.conn.commit()
+        listescore = self.cur.fetchall()
         return listescore
+
+    def supprimer(self,trucasupprimer):
+        suppr = trucasupprimer
+        self.cur.execute('DELETE FROM TOP_SCORE WHERE nom = ?', suppr)
+        self.conn.commit()
+
+
     def fin_programme(self):
-        cur.close()
-        conn.close()
+        self.cur.close()
+        self.conn.close()
+
+b1 = p4_basesdedonnee()
+b1.ajoutdejoueuroumodificationdelabasededonee("josh", 12)
+print(b1.recupererlesmeilleursscores())
+b1.supprimer("josh")
+b1.fin_programme()
