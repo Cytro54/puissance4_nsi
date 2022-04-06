479d59907b13b45a2c7a0950d48296790d0ac92b
Yanis CHERIBET <cheribey@s212-08.eaux-claires.net>
2022-04-06 10:24:00 +0200

    fonction victoire

--- modules/p4_game.py
+++ modules/p4_game.py
@@ -1,3 +1,4 @@
+import p4_ia
 #variables principales
 
 nb_line = 6 # hauteur du plateau
@@ -8,9 +9,9 @@ vide, j1, j2 = 0, 1, 2 # diférenciation des cases du joueur1, du joueur 2 et de
 j1_score, j2_score = 0, 0
 
 class P4_game():
-   """
-   classe qui va s'occuper de la création du jeu
-   """ 
+    """
+    classe qui va s'occuper de la création du jeu
+    """ 
     def __init__(self):
         self.plateau = []
         self.tour = 0 #si le tour est un nombre paire le joueur 1 joue, sinon c'est le joueur 2 qui joue  
@@ -29,7 +30,7 @@ class P4_game():
 
         return self.plateau
 
-    def get_case(line,col):
+    def get_case(self,line,col):
         return self.plateau[col][line]
 
 
@@ -55,13 +56,13 @@ class P4_game():
         entrée: le numéro de la colonne
         sortie: le jeton correspondant au joueurs 1 ou 2 serra ajouté au dessus des autres jetons de la colonne
         """
-        if self.jeu_possible() = False:
+        if self.jeu_possible() == False:
             print("la colonne est pleine")
 
         elif self.tour % 2 == 0:
             for i in self.plateau[col]:
                 while i >= nb_line:
-                    if self.plateau[col][i] = 0:
+                    if self.plateau[col][i] == 0:
                         self.plateau[col][i] = 1
 
             self.tour +=1
@@ -69,25 +70,17 @@ class P4_game():
         else:
             for i in self.plateau[col]:
                 while i >= nb_line:
-                    if self.plateau[col][i] = 0:
+                    if self.plateau[col][i] == 0:
                         self.plateau[col][i] = 2
 
             self.tour +=1
-        return self.tour, self.plateau
+        return (self.tour, self.plateau)
 
     def victoire(self):
-        """
-        fonction qui va déterminer qui à gagner la partie et qui va ajouter 1 au score du gagnant
-        """
-        while:
-
-            if tour %2 = 0:
-                if somme = nb_winer_line:
-                    return j1_score +1
-            else:
-                if somme = nb_winer_line:
-                    return j1_score +1
-        
+        if self.tour % 2 == 0: #tour du joueur 1
+            return p4_ia.a_gagne(self, 1)
+        else: #tour du joueur 2
+            return p4_ia.a_gagne(self, 2)
 
 
     
@@ -95,4 +88,4 @@ class P4_game():
         """
         renvoie le score des deux joueurs
         """
-        return self.j1_score, self.j2_score
+        return (self.j1_score, self.j2_score)
