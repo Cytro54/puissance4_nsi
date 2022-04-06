677e331e9e80bd65b547ac10f9f7a346584147e5
Cytro54 <filoue@s212-07.eaux-claires.net>
2022-04-06 10:38:02 +0200

    Avancement du Module réseau

--- modules/p4_network.py
+++ modules/p4_network.py
@@ -2,21 +2,15 @@
 # au réseau. Un (mauvais) systéme de RPC sera 
 # implémenté pour faire fonctionner le tout
 
+import json
 import socket
+import socketserver
+from subprocess import call
 
 TAILLE_BUFFER = 1024
-SEPARATEUR = b'\n'
+SEPARATEUR = b'&'
+SEPARATEUR_INT = 38
 
-def __yuri_rpc_attendre_frame(socket):
-    ## ATTENTION : Cette fonction bloque le thread sur lequel elle est executée
-    frame = bytearray()
-    while True:
-        data = socket.recv(TAILLE_BUFFER)
-        if not data: 
-            break
-        else:
-            frame.append(data)
-    return frame
 
 #
 # Yuri Rpc ABI:
@@ -35,68 +29,67 @@ def __yuri_rpc_attendre_frame(socket):
 # }
 # 
 
-def __yuri_rpc_decompose_frame(frame):
-    buff = bytearray()
-    for b in frame:
-        if b == SEPARATEUR:
-            yield buff.decode('utf-8')
-            buff = bytearray()
-        else:
-            buff.append(b)
-    if buff:
-        yield buff.decode('utf-8') 
+class YuriRPCServerGenericHandler(socketserver.BaseRequestHandler):
+    def __init__(self, options):
+        super().__init__(self)
+        self._opts = options
+        self._codec = 'utf-8'
 
-def __yuri_rpc_message_depuis_frame(frame):
-    is_key = True
-    key_buffer = None
-    message = {}
-    for line in __yuri_rpc_decompose_frame(frame):
-        if is_key: 
-            key_buffer = line
-        else: 
-            message.update({key_buffer: line})
-        is_key = not is_key
-    return message
-
-def __yuri_rpc_message_vers_frame(message):
-    stream = bytearray()
-    i = len(message)
-    y = 1
-    for key in message:
-        # Ajouter une ligne
-        stream += bytes(key, 'utf-8')
-        stream += SEPARATEUR
-        stream += bytes(message[key], 'utf-8')
-        if not (i == y):
-            stream += SEPARATEUR
-            y += 1
-    return stream
-
-class YuriRPCClient:
-    def __init__(self):
-        pass
     
-    async def connecter(self, addresse):
-        pass
+    def __attendre_frame(self, socket):
+        ## ATTENTION : Cette fonction bloque le thread sur lequel elle est executée
+        frame = bytearray()
+        while True:
+            data = socket.recv(TAILLE_BUFFER)
+            if not data:
+                break
+            else:
+                frame.append(data)
+        return frame
 
-    async def appler(self, nom, parametres):
-        pass
-class YuriRPCServer:
-    def __init__(self):
-        pass
 
-    async def ecouter(self, port):
-        pass
+    def __message_depuis_frame(self, frame):
+        return json.load(frame.decode(self._codec))
 
-    def ajouter_fn(self, nom, fonction):
-        pass
+    def __message_vers_frame(self, message):
+        return bytes(json.dump(message), self._codec)
+
+
+    def handle(self):
+        # self.request is the TCP socket connected to the client
+        frame = self.__attendre_frame(self.request)
+        message = self.__message_depuis_frame(frame)
+        # Interpretons le message
+        method = message.get('method', None)
+        params = message.get('params', None)
+        call_id = message.get('id', None)
+        if method != None and params != None and call_id != None:
+            py_method = f"do_{method}"
+            res = self[py_method](*params)
+            message = {'id': call_id, 'response': res}
+            frame = self.__message_vers_frame(message)
+            self.request.sendall(frame)
+        else:
+            pass
+            # Message invalide, droppons le
+
+def init_serveur_rpc(host, port, fonctions):
+    with socketserver.TCPServer((host, port), YuriRPCServerHandler) as server:
+        server.serve_forever()
+
+
+
 
 if __name__ == "__main__":
     # Quelques Asserts
+
+    # 1. Test de sanité
     message_a_encoder = {'test': 'value', 'cool': 'code'}
     frame = __yuri_rpc_message_vers_frame(message_a_encoder)
     message_decode = __yuri_rpc_message_depuis_frame(frame)
+    print("A encoder :")
     print(message_a_encoder)
+    print("Décodé :")
     print(message_decode)
     assert message_a_encoder == message_decode
-       
+    # 2. 
