7e7b34a9399edd4f4b11e361cadd6dc9a07039a2
Cytro54 <filoue@s212-07.eaux-claires.net>
2022-04-06 11:16:21 +0200

    Je hais python

--- puissance4.py
+++ puissance4.py
@@ -1,9 +1,17 @@
+import pathlib
+import site
+
+# Python je te hais de maniére passionelle; je suis pas censé faire ca :(
+relative_home = pathlib.Path(__file__).parent.absolute()
+module_home = relative_home.joinpath("modules")
+site.addsitedir(module_home)
+
+
 from modules.p4_basesdedonnee import *
 from modules.p4_console import *
 from modules.p4_game import *
 from modules.p4_ia import *
 from modules.p4_network import *
-
 
 p4g = P4_game()
 p4g.jeu()
