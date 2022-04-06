71b02d0a08ff9712e72517053df5f63946f0c6c8
TheGreatestRock <vieux-mv@s212-01.eaux-claires.net>
2022-04-06 10:24:58 +0200

    quelques modif

--- modules/p4_console.py
+++ modules/p4_console.py
@@ -9,10 +9,13 @@ from p4_basesdedonnee import *
 class p4_console:
     def __init__(self):
         pass
+    #affiche le plateau de jeu
     def affiche(self,plateau):
+        #affiche le haut du plateau, les joueurs et les scores
         print(f"┏━━━━━━━━━━━━━━━━━━━━━━━━━━━┓     Score {j1['nom']} : {j1['score']}")
         print(f"┃         Puissance 4       ┃     Score {j2['nom']} : {j2['score']}")
         print("┃━━━━━━━━━━━━━━━━━━━━━━━━━━━┃")
+        #commence a afficher la "vrai" partie du tableau
         for i in plateau:
             print("┃ ", end="")
             for y in range(len(i)):
@@ -24,6 +27,7 @@ class p4_console:
                     print(f"{j2['motif']}┃", end="")
                 print(" ┃")
                 print("┃━━━╋━━━╋━━━╋━━━╋━━━╋━━━╋━━━┃")
+        #affiche le numéro des colonnes du plateau
         print("┃━━━━━━━━━━━━━━━━━━━━━━━━━━━┃")
         print("┃ 1 ┃ 2 ┃ 3 ┃ 4 ┃ 5 ┃ 6 ┃ 7 ┃")
         print("┗━━━━━━━━━━━━━━━━━━━━━━━━━━━┛")
@@ -34,15 +38,16 @@ class joueur:
         pass
 
     def debut_jeu(self):
-
+        #fait ce qui se passe au debut du jeu
         nb_joueurs = 0
         while nb_joueurs != 1 or nb_joueurs != 2 :
             print("Mode de jeux :")
             print("1: joueur contre IA")
             print("2: joueur contre joueur")
             nb_joueurs = int(input())
-            if nb_joueurs != 1 or nb_joueurs != 2 :
+            if nb_joueurs != 1 or nb_joueurs != 2:
                 print("erreur")
+        #si il y a 1 joueur contre une IA
         if nb_joueurs == 1 :
             j1["nom"] = input("nom du joueur :")
             j2["nom"] = "Viktor Nikiforov"    
@@ -60,24 +65,34 @@ class joueur:
             motif2 = lmotif[int(input())]
             j1["motif"] = motif1
             j2["motif"] = motif2
-
+        #si il y a 2 joueurs, demande is le jeu est en ligne ou en local
         if nb_joueurs == 2 :
-            j1["nom"] = input("nom du joueur 1 :")
-            j2["nom"] = input("nom du joueur 2 :")
-            j1["score"] = 0
-            j2["score"] = 0
-            lmotif = ["⯀","⯁","⯂","⯄","⯅"]
-            print("motif du joueur 1 :")
-            for i in range(len(lmotif)):
-                print(f"{i} : {lmotif[i]}")
-            motif1 = lmotif[int(input())]
-            lmotif.remove(motif1)
-            print("motif du joueur 2 :")
-            for i in range(len(lmotif)):
-                print(f"{i} : {lmotif[i]}")
-            motif2 = lmotif[int(input())]
-            j1["motif"] = motif1
-            j2["motif"] = motif2
+            while nb_joueurs != 1 or nb_joueurs != 2 :
+                print("Mode de jeux :")
+                print("1: en ligne")
+                print("2: en local")
+                type_de_jeu = int(input())
+                if type_de_jeu != 1 or type_de_jeu != 2:
+                    print("erreur")
+            if type_de_jeu == 1:
+                print("marche pas encore")         
+            if type_de_jeu == 2:
+                j1["nom"] = input("nom du joueur 1 :")
+                j2["nom"] = input("nom du joueur 2 :")
+                j1["score"] = 0
+                j2["score"] = 0
+                lmotif = ["⯀","⯁","⯂","⯄","⯅"]
+                print("motif du joueur 1 :")
+                for i in range(len(lmotif)):
+                    print(f"{i} : {lmotif[i]}")
+                motif1 = lmotif[int(input())]
+                lmotif.remove(motif1)
+                print("motif du joueur 2 :")
+                for i in range(len(lmotif)):
+                    print(f"{i} : {lmotif[i]}")
+                motif2 = lmotif[int(input())]
+                j1["motif"] = motif1
+                j2["motif"] = motif2
 
 
 
