2b5959a7f686bc5596929dc85e1bda2059e63b96
Yanis CHERIBET <cheribey@s212-08.eaux-claires.net>
2022-04-08 11:21:52 +0200

    reparation de placer

--- modules/test.py
+++ modules/test.py
@@ -1,43 +1,8 @@
-#juste pour tester mon code
-tour = 0
-nb_line = 6
-nb_col = 7
 
-
-tour = 0
-nb_line = 6
-nb_col = 7
-col = 1
-
-plateau = [
-    [0, 0, 0, 0, 0, 0],     # |
-    [0, 0, 0, 0, 0, 0],     # | "Ligne"
-    [0, 0, 0, 0, 0, 0],     # V
-    [0, 0, 0, 0, 0, 0],     # + ----->
-    [0, 0, 0, 0, 0, 0],     #    "Colonne"
-    [0, 0, 0, 0, 0, 0],     #
-]
-
+plateau = []
+for _ in range(6):
+    nvline = []
+    for _ in range(7):
+        nvline.append(0)
+    plateau.append(nvline)
 print(plateau)
-
-if tour % 2 == 0:
-
-    for i in plateau[col]:
-        
-        while plateau[col][i] >= nb_line:
-            if len(plateau[col]) => 6
-            
-            if i != 0:
-                plateau[col][i-1] = 1
-            else:
-                pass
-    tour +=1
-
-else:
-    for i in plateau[col]:
-        while plateau[col][i] >= nb_line:
-            if i != 0:
-                plateau[col][i-1] = 2
-            else:
-                pass
-    tour +=1
